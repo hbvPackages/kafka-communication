7531b8f1fb27cb04355dfe2346867760f6c3f5b4
hbv73 <vahid.habibzadeh73@gmail.com>
2024-12-19 00:10:13 +0330

    update provider & command

--- src/Console/ConsumeCommand.php
+++ src/Console/ConsumeCommand.php
@@ -7,7 +7,7 @@ use Hbv\KafkaCommunication\Services\Communicate;
 
 class ConsumeCommand extends Command
 {
-    protected $signature = 'KafkaCommunication:consume {topic}';
+    protected $signature = 'kcommunication:consume {topic}';
     protected $description = 'Consume messages from Kafka topic and send response to dynamic topic';
 
     public function handle()
